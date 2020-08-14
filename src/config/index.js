c7884336e8b68ae349030cbed9c778c3edaa783c
Thiago Silva <thiago25pro@gmail.com>
2020-08-13 21:57:18 -0300

    bar removed from config url

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-const URL_PRODUCAO = 'https://ancapflix.herokuapp.com/';
+const URL_PRODUCAO = 'https://ancapflix.herokuapp.com';
 const URL_DEV = 'http://localhost:8080';
 
 const URL_TOP = window.location.hostname.includes('localhost') ? URL_DEV : URL_PRODUCAO;
