5134828f66a8735c50dde0b558f56a03331d334a
Thiago Silva <thiago25pro@gmail.com>
2020-08-13 22:17:12 -0300

    a link changed

--- src/components/Menu/components/ButtonLink/index.js
+++ src/components/Menu/components/ButtonLink/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import url_top from '../../../../config/';
+
 const ButtonLink = (props) => {
-    return(
-        <a href={props.href} className={props.className}>
-            { props.children }
-        </a>
-    )
+    const { URL_TOP } = url_top;
+
+    return (URL_TOP.search('localhost') ? <a href={props.href} className={props.className}>  { props.children } </a> : window.alert('Action not permitted'))
+    
 };
 
 export default ButtonLink;
