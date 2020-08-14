c0118aa9a99b63a419dbd9f4339823c9071a0093
Thiago Silva <thiago25pro@gmail.com>
2020-08-13 21:41:35 -0300

    new folders

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,8 +10,9 @@ function Footer() {
       <p>
         Orgulhosamente criado durante a
         {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
+        Imersão React da Alura por {' '}
+        <a href="https://github.com/pgThiago">
+          Thiago Silva
         </a>
       </p>
     </FooterBase>
